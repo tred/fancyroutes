5547de3a7953ed637a97300712243061c4cf51bf
Carl Woodward <carl@carl-woodwards-macbook.local>
2008-12-14 10:48:10 +1100

    Initial commit moved out of tred repo and into plugin for rails

---
+++ lib/fancyroutes.rb
@@ -0,0 +1,107 @@
+module FancyRoutes
+  class Route
+    
+    class << self
+      def copy(map,segments,controller,action)
+        copy = Route.new(map)
+        copy.segments = segments.dup unless segments.nil?
+        copy.controller(controller.dup) unless controller.nil?
+        copy.action(action.dup) unless action.nil?
+        copy
+      end
+    end
+    
+    def initialize(map)
+      @map = map
+      @segments = []
+    end
+
+    def request_method(method)
+      @request_method = method.to_s
+      self
+    end
+    
+    def segment(path_segment)
+      @segments << path_segment
+      self
+    end
+    alias_method :/, :segment
+    
+    def segments=(segments)
+      @segments = segments
+    end
+    
+    def controller(controller_name)
+      @controller = controller_name.to_s
+      self
+    end
+    alias_method :>>, :controller
+    
+    def action(action_name)
+      @action = action_name.to_s
+      self
+    end
+    alias_method :>, :action
+    
+    %w(get post put delete).each do |verb|
+      define_method verb do
+        c = copy
+        c.request_method(verb.to_sym)
+        c
+      end
+    end
+    
+    def with
+      self
+    end
+    
+    def match(&blk)
+      instance_eval &blk
+    end
+        
+    def create
+      @map.connect(segments, 
+        :controller => @controller,
+        :action => @action,
+        :conditions => { :method => @request_method })
+    end
+    
+    protected
+    
+    def segments
+      @segments = @segments.collect do |segment|
+        case segment
+        when String
+          segment
+        when Symbol
+          (':' + segment.to_s)
+        when Hash
+          key, value = segment.keys.first, segment.values.first
+          hash_segment key, value
+        end 
+      end.join('/')
+    end
+    
+    def hash_segment(key,value)
+      if value == :controller
+        controller key
+      elsif value == :action
+        action key
+      end
+      key
+    end
+    
+    def copy
+      Route.copy @map, @segments, @controller, @action
+    end
+     
+  end
+  
+  class RouteSet
+    def initialize(map,&blk)
+      @map = map
+      route = Route.new map
+      route.instance_eval &blk
+    end
+  end
+end
---
+++ test/fancyroutes_test.rb
@@ -0,0 +1,126 @@
+require "rubygems"
+require "spec"
+require File.expand_path(File.dirname(__FILE__) + "/../lib/fancyroutes")
+
+Spec::Runner.configure do |config|
+  config.mock_with :rr
+end
+
+describe "FancyRoutes" do
+  
+  before(:each) do
+    @map = Object.new
+    @fancyroutes = FancyRoutes::Route.new(@map)
+  end
+
+  example "route on root" do
+    mock(@map).connect('', {
+      :controller => 'my_controller',
+      :action => 'my_action',
+      :conditions => { :method => 'get' },
+    })
+    
+    @fancyroutes.request_method(:get).segment('').controller(:my_controller).action(:my_action).create
+  end  
+
+  example "short form route on root" do
+    mock(@map).connect('', {
+      :controller => 'my_controller',
+      :action => 'my_action',
+      :conditions => { :method => 'get' },
+    })
+    
+    (@fancyroutes.get / '' >> :my_controller > :my_action).create
+  end
+  
+  example "short form route on order" do
+    mock(@map).connect(':slug/order', {
+      :controller => 'my_controller',
+      :action => 'my_action',
+      :conditions => {:method => 'get'}
+    })
+    
+    (@fancyroutes.get / :slug / 'order' >> :my_controller > :my_action).create
+  end
+  
+  example "short form route on order as post" do
+    mock(@map).connect(':slug/order', {
+      :controller => 'my_controller',
+      :action => 'my_action',
+      :conditions => {:method => 'post'}
+    })
+    
+    (@fancyroutes.post / :slug / 'order' >> :my_controller > :my_action).create
+  end
+  
+  example "short form route nested route" do
+    mock(@map).connect(':slug/order', {
+      :controller => 'my_controller',
+      :action => 'my_action',
+      :conditions => {:method => 'get'}
+    })
+    mock(@map).connect(':slug/order', {
+      :controller => 'my_controller',
+      :action => 'my_action',
+      :conditions => {:method => 'post'}
+    })
+    
+    (@fancyroutes.with / :slug >> :my_controller).match do
+      (get / 'order' > :my_action).create
+      (post / 'order' > :my_action).create
+    end 
+  end
+  
+  example "nested in nested routes" do
+    mock(@map).connect(':slug/order', {
+      :controller => 'my_controller',
+      :action => 'my_action',
+      :conditions => {:method => 'get'}
+    })
+    mock(@map).connect(':slug/order', {
+      :controller => 'my_controller',
+      :action => 'my_action',
+      :conditions => {:method => 'post'}
+    })
+    
+    (@fancyroutes.with / :slug).match do
+      (with >> :my_controller).match do
+        (get / 'order' > :my_action).create
+        (post / 'order' > :my_action).create
+      end
+    end 
+  end
+  
+  example "testing route set" do
+    mock(@map).connect(':slug/order', {
+      :controller => 'my_controller',
+      :action => 'my_action',
+      :conditions => {:method => 'get'}
+    })
+    mock(@map).connect(':slug/order', {
+      :controller => 'my_controller',
+      :action => 'my_action',
+      :conditions => {:method => 'post'}
+    })
+    
+    FancyRoutes::RouteSet.new(@map) do
+      (with / :slug).match do
+        (with >> :my_controller).match do
+          (get / 'order' > :my_action).create
+          (post / 'order' > :my_action).create
+        end
+      end
+    end 
+  end
+  
+  example "with hashes for controller" do
+    mock(@map).connect('item_images/:image', {
+      :controller => 'item_images',
+      :action => 'show',
+      :conditions => {:method => 'get'}
+    })
+    
+    (@fancyroutes.get / {'item_images' => :controller} / :image > :show).create
+  end
+  
+end
